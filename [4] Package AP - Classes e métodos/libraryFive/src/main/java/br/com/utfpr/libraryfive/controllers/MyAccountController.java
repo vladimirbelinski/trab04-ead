63c1dd5aff4caef15f365470e720a412371044be
Lucas Zarzur <lucas12zarzur@gmail.com>
2020-05-18 23:56:10 -0300

    Implements "My Account" page, only with info

--- [4] Package AP - Classes e métodos/libraryFive/src/main/java/br/com/utfpr/libraryfive/controllers/MyAccountController.java
+++ [4] Package AP - Classes e métodos/libraryFive/src/main/java/br/com/utfpr/libraryfive/controllers/MyAccountController.java
@@ -1,11 +1,56 @@
 package br.com.utfpr.libraryfive.controllers;
 
+import br.com.utfpr.libraryfive.model.LoanModel;
+import br.com.utfpr.libraryfive.model.ReturnModel;
+import br.com.utfpr.libraryfive.model.UserModel;
+import br.com.utfpr.libraryfive.service.LoanService;
+import br.com.utfpr.libraryfive.service.ReturnService;
+import br.com.utfpr.libraryfive.util.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Transactional
 @RequestMapping("/myAccount")
 public class MyAccountController {
+
+    @Autowired
+    private Session session;
+
+    @Autowired
+    private LoanService loanService;
+
+    @Autowired
+    private ReturnService returnService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView showMyAccountDetails() {
+
+        UserModel user = session.getCurrentUser();
+
+        List<ReturnModel> returnedLoans = returnService.findAllReturnedLoansByEmail(session.getCurrentUser().getEmail());
+        List<LoanModel> activeLoans = loanService.listAllByEmail(user.getEmail());
+        List<LoanModel> overdueLoans = new ArrayList<>();
+
+        for (LoanModel loanModel : activeLoans) {
+            if(loanService.isLoanLate(loanModel)) {
+                overdueLoans.add(loanModel);
+            }
+        }
+
+        ModelAndView modelAndView = new ModelAndView("user/myAccount");
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("allLoans", returnedLoans.size() + activeLoans.size());
+        modelAndView.addObject("activeLoans", activeLoans.size());
+        modelAndView.addObject("overdueLoans", overdueLoans.size());
+
+        return modelAndView;
+    }
 }
